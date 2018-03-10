fe3d0a3a2b501bd9313efc592bd746aab45a3388
Artem Litvinov <artem@craft.co>
2018-03-10 17:39:28 +0300

    Fixed output encoding issue

--- parsers/onliner/onliner/settings.py
+++ parsers/onliner/onliner/settings.py
@@ -88,3 +88,6 @@ HTTPCACHE_EXPIRATION_SECS = 84600
 HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+FEED_EXPORT_ENCODING = 'utf-8'
--- parsers/onliner/onliner/spiders/realt.py
+++ parsers/onliner/onliner/spiders/realt.py
@@ -11,7 +11,7 @@ class RealtSpider(scrapy.Spider):
     name = "onliner.realt"
     allowed_domains = ["pk.api.onliner.by"]
 
-    start_urls = ['https://pk.api.onliner.by/search/apartments?bounds%5Blb%5D%5Blat%5D=53.650335967826045&bounds%5Blb%5D%5Blong%5D=27.33123779296875&bounds%5Brt%5D%5Blat%5D=54.14433895052212&bounds%5Brt%5D%5Blong%5D=27.793350219726566&page={}'.format(page) for page in range(1, 42)]
+    start_urls = ['https://pk.api.onliner.by/search/apartments?bounds%5Blb%5D%5Blat%5D=53.650335967826045&bounds%5Blb%5D%5Blong%5D=27.33123779296875&bounds%5Brt%5D%5Blat%5D=54.14433895052212&bounds%5Brt%5D%5Blong%5D=27.793350219726566&page={}'.format(page) for page in range(1, 43)]
 
     def parse(self, response):
         response_data = json.loads(response.body)
