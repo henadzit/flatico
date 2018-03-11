43d1841c9c8acfd39cd2d2db9075c0b3d32c90d0
Anthony Marchenko <anthonymarchenko@gmail.com>
2018-03-11 14:49:01 +0300

    fix issue with parameters type

--- ios/Flatico/LottieAnimationViewController.swift
+++ ios/Flatico/LottieAnimationViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Lottie
 import Alamofire
 
-class LottieAnimationViewController: UIViewController, URLSessionDownloadDelegate {
+class LottieAnimationViewController: UIViewController {
     
     private var downloadTask: URLSessionDownloadTask?
     private var boatAnimation: LOTAnimationView?
@@ -52,76 +52,68 @@ class LottieAnimationViewController: UIViewController, URLSessionDownloadDelegat
         
     }
     
-    @objc func startDownload(button: UIButton) {
-        button.isHidden = true
-        createDownloadTask()
-    }
-    
-    func createDownloadTask() {
-        let downloadRequest = URLRequest(url: URL(string: "https://upload.wikimedia.org/wikipedia/commons/8/8f/Whole_world_-_land_and_oceans_12000.jpg")!)
-        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: OperationQueue.main)
-        
-        downloadTask = session.downloadTask(with:downloadRequest)
-        downloadTask!.resume()
-    }
     
     func createUploadTask() {
-        let yourUrl = "http://159.65.94.180/v1/predict-price"
         
-        let imageToUpload = UIImage(named:"startBackgroundImage")
-        let params: Parameters = ["rooms_count": "1", "floor": "2", "has_balcony" : "true", "total_square" : "22", "latitude" : "53.1", "longitude" : "57" ]
-        Alamofire.upload(multipartFormData:
-            {
-                (multipartFormData) in
-                multipartFormData.append(UIImageJPEGRepresentation(imageToUpload!, 0.1)!, withName: "photo_0", fileName: "file.jpeg", mimeType: "image/jpeg")
-                for (key, value) in params
-                {
-                    multipartFormData.append((value as AnyObject).data(using: String.Encoding.utf8.rawValue)!, withName: key)
+        let url = "http://159.65.94.180/v1/predict-price"
+        
+        let imageToUpload = UIImage(named:"startBackgroundImage")!
+        let imageData = UIImageJPEGRepresentation(imageToUpload, 1.0)
+        
+        let headers: HTTPHeaders = [
+            "Content-type": "multipart/form-data"
+        ]
+        
+        let parameters: Parameters = ["rooms_count": 1, "floor": 2, "has_balcony" : true, "total_square" : 22, "latitude" : 53.1, "longitude" : 57.44]
+        
+        Alamofire.upload(multipartFormData: { (multipartFormData) in
+            for (key, value) in parameters {
+                multipartFormData.append("\(value)".data(using: String.Encoding.utf8)!, withName: key as String)
+            }
+            
+            if let data = imageData {
+                multipartFormData.append(data, withName: "photo_0", fileName: "image.png", mimeType: "image/png")
+            }
+            
+        }, usingThreshold: UInt64.init(), to: url, method: .post, headers: headers) { (result) in
+            switch result{
+            case .success(let upload, _, _):
+                upload.responseJSON { response in
+                    print("Succesfully uploaded")
+                    print("\(response)")
+                    if let err = response.error{
+                        //  onError?(err)
+                        return
+                    }
+                    //   onCompletion?(nil)
                 }
-        }, to:yourUrl,headers:nil)
-        { (result) in
-            switch result {
-            case .success(let upload,_,_ ):
-                upload.uploadProgress(closure: { (progress) in
-                    //Print progress
-                })
-                upload.responseJSON
-                    { response in
-                        //print response.result
-                        if response.result.value != nil
-                        {
-                            let dict :NSDictionary = response.result.value! as! NSDictionary
-                            let status = dict.value(forKey: "status")as! String
-                            if status=="1"
-                            {
-                                print("DATA UPLOAD SUCCESSFULLY")
-                            }
-                        }
-                }
-            case .failure(let _):
-                
-                break
+            case .failure(let error):
+                print("Error in upload: \(error.localizedDescription)")
+                // onError?(error)
             }
         }
     }
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        // Pause the animation and disable looping.
-        boatAnimation!.pause()
-        boatAnimation!.loopAnimation = false
-        // Speed up animation to finish out the current loop.
-        boatAnimation!.animationSpeed = 4
-        
-        boatAnimation!.play(toProgress: 0.5) {[weak self] (_) in
-            // At this time the animation is at the halfway point. Reset sped to 1 and play through the completion animation.
-            self?.boatAnimation!.animationSpeed = 1
-            self?.boatAnimation!.play(toProgress: 1, withCompletion: nil)
-        }
-    }
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        positionInterpolator?.currentProgress = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
-    }
-    
 }
+
+
+
+
+//    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+//        // Pause the animation and disable looping.
+//        boatAnimation!.pause()
+//        boatAnimation!.loopAnimation = false
+//        // Speed up animation to finish out the current loop.
+//        boatAnimation!.animationSpeed = 4
+//
+//        boatAnimation!.play(toProgress: 0.5) {[weak self] (_) in
+//            // At this time the animation is at the halfway point. Reset sped to 1 and play through the completion animation.
+//            self?.boatAnimation!.animationSpeed = 1
+//            self?.boatAnimation!.play(toProgress: 1, withCompletion: nil)
+//        }
+//    }
+//
+//    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+//        positionInterpolator?.currentProgress = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
+//    }
+
 
