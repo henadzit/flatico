44d0ea6b16dc5b56672c831abfa081d61afa8c5b
Artem Litvinov <artem@craft.co>
2018-03-11 15:25:40 +0300

    Added imput params capturing

--- backend/app.py
+++ backend/app.py
@@ -6,13 +6,26 @@ from aiohttp import web
 # floor
 # has_balcony
 # total_square
+# latitude
+# longitude
 
 async def handle_predict(request):
-    # data = await request.post()
+    data = await request.post()
+    model_input_params = {
+        'rooms_count': int(data.get('rooms_count', None)),
+        'floor': int(data.get('floor', None)),
+        'has_balcony': bool(data.get('has_balcony', None)),
+        'total_square': float(data.get('total_square', None)),
+        'latitude': float(data.get('latitude', None)),
+        'longitude': float(data.get('longitude', None)),
+    }
+    print(model_input_params)
+    
+    
     response_data = { 'predicted_price': { 'value': 542345, 'currency': 'USD' }}
     return web.json_response(response_data)
 
 app = web.Application()
 app.router.add_post('/v1/predict-price', handle_predict)
 
-web.run_app(app, port=80)
+web.run_app(app, port=8080)
