79512245a8fa24895ffc0c17990cd19e25f1d166
freemanoid <freemanoid321@gmail.com>
2018-03-11 16:45:29 +0300

    Use apt_state for price calculation

--- regression/example.py
+++ regression/example.py
@@ -1,7 +1,7 @@
 from catboost import CatBoostRegressor
 
-model = CatBoostRegressor().load_model("zaebis_model")
+model = CatBoostRegressor().load_model("fitted_model")
 
-# floor_number, total_floors, area, latitude, longitude
+# floor_number, total_floors, area, latitude, longitude, apt_state
 
-model.predict([[2, 5, 45, 53.908681, 27.572759]])
+model.predict([[2, 5, 45, 53.908681, 27.572759, 1]])
