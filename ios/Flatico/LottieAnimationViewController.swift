9217a2b55a06c8c1e0d2566f3869db0d9333c194
Anthony Marchenko <anthonymarchenko@gmail.com>
2018-03-11 11:18:28 +0300

    ios project init

---
+++ ios/Flatico/LottieAnimationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LottieAnimationViewController.swift
+//  Flatico
+//
+//  Created by Anthony Marchenko on 3/11/18.
+//  Copyright © 2018 Anthony Marchenko. All rights reserved.
+//
+
+import UIKit
+
+class LottieAnimationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
