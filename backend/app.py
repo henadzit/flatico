fdfd4c5ea810ff03dde2612128d4d759a5dffb6e
Artem Litvinov <artem@craft.co>
2018-03-11 10:50:13 +0300

    Fixed syntax issue in backend, change port to 80

--- backend/app.py
+++ backend/app.py
@@ -9,9 +9,9 @@ from aiohttp import web
 
 async def handle_predict(request):
     # data = await request.post()
-    return web.Response(text={ predicted_price: { value: 542345, currency: 'USD' }})
+    return web.Response(text={ 'predicted_price': { 'value': 542345, 'currency': 'USD' }})
 
 app = web.Application()
 app.router.add_get('/v1/predict-price', handle_predict)
 
-web.run_app(app)
+web.run_app(app, port=80)
