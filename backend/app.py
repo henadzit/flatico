f738e70ad1e40f9cea3008409aba961b62647eea
henadzit <henadzi.tsaryk@gmail.com>
2018-03-11 17:21:23 +0300

    Add predictor

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ import io
 
 from classifier import Classifier
 
+from catboost import CatBoostRegressor
+
 import tempfile
 
 ### Request format:
@@ -16,6 +18,14 @@ import tempfile
 
 
 image_classifier = Classifier()
+model = CatBoostRegressor().load_model("fitted_model")
+
+
+CLASS_TO_APT_STATE = {
+    'netotd': 1,
+    'bab': 2,
+    'remont': 3
+}
 
 
 async def handle_predict(request):
@@ -35,13 +45,23 @@ async def handle_predict(request):
     with tempfile.NamedTemporaryFile(mode="wb", delete=True) as img:
         img.write(uploaded_file_reader.read())
         img.seek(0, 0)
-        
+
         classification_res = image_classifier.classify(img.name)[0]
-        model_input_params['apt_state'] = classification_res[0]
+        model_input_params['apt_state'] = CLASS_TO_APT_STATE[classification_res[0]]
 
     print(model_input_params)
-    
-    response_data = { 'predicted_price': { 'value': 542345, 'currency': 'USD' }, 'input_params': model_input_params }
+
+    # TODO: 10 is the hardcoded number of floors
+    price = model.predict([[model_input_params['floor'],
+                            10,
+                            model_input_params['total_square'],
+                            model_input_params['latitude'],
+                            model_input_params['longitude'],
+                            model_input_params['apt_state']]])[0]
+
+    # import pdb; pdb.set_trace()
+
+    response_data = { 'predicted_price': { 'value': price, 'currency': 'USD' }, 'input_params': model_input_params }
     return web.json_response(response_data)
 
 app = web.Application(client_max_size=10_000_000)
